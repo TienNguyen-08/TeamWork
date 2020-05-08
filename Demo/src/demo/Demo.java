f9f0896a6e6f99e49d70507e3eae57af2e255109
TienNguyen-08 <cnnguyenvantien@gmail.com>
2020-05-08 08:48:52 +0700

    firt commit

---
+++ Demo/src/demo/Demo.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Demo {
+
+	public void Demo1() {
+		System.out.println("demo for git-hub");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo demo = new Demo();
+		demo.Demo1();
+	}
+
+}
